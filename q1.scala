c8c3177aae039a0a7b6e55231833c826c0f3d129
Oshada Rodney <84395086+SuhadaKOKA@users.noreply.github.com>
2022-07-29 01:27:38 +0530

    Add files via upload

---
+++ q1.scala
@@ -0,0 +1,12 @@
+object q1 extends App(){
+    def interest(amount:Double):Double = amount match{
+        case x if x<20000 => x*0.02
+        case x if x<200000 => x*0.04
+        case x if x<2000000 => x*0.035
+        case x if x>2000000 => x*0.065
+    }
+
+    var a:Int = 250000;
+    println(interest(a));
+    
+}
